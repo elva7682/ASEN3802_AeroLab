cd9e11da4e901e82fb1efa13fe00bb5850fdf6a8
elva7682 <elva7682@colorado.edu>
2026-04-07 12:39:41 -0600

    updated naca

---
+++ main_fake.m
@@ -0,0 +1,25 @@
+
+clc
+clear
+close all
+
+
+
+
+
+%% Task 1
+c = 1;
+N = 50;
+m = 0;
+p = 0;
+t = 12;
+
+[x, y] = NACA_Airfoils(m,p,t,c,N);
+
+
+
+%% Task 3
+
+% Plot predicted cl over a range of alpha for each 
+naca_0006 = load("0006.mat");
+naca_0012 = load("0012.mat");
