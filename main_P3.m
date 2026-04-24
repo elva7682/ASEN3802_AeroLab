563012142d1b13d323574f03a5b14323601affc0
KAVI2978 <149836305+KAVI2978@users.noreply.github.com>
2026-04-24 12:31:49 -0600

    Add files via upload

--- main_P3.m
+++ main_P3.m
@@ -17,16 +17,18 @@ b = 33 + 4/12; % [ft]
 c_r = 5 + 4/12; % [ft]
 c_t = 3 + 8.5/12; % [ft]
 
-alpha = 4; % [rad]
+alpha = 4; % [deg]
 
 [cl_r, aero_r] = ThinAirfoil(c_r,m_r,p_r,Np,alpha);
 [cl_t, aero_t] = ThinAirfoil(c_t,m_t,p_t,Np,alpha);
 
-geo_r = 1; % [deg]
-geo_t = 0; % [deg]
+geo_r = 5; % [deg]
+geo_t = 4; % [deg]
 
-a0_r = 0.1033; % [rad^-1] 
-a0_t = 0.1059; % [rad^-1]
+a0_r = 0.11727*180/pi; % [rad^-1] 
+a0_t = 0.11744*180/pi; % [rad^-1] % symmetric airfoil
+
+S = c_t*b + 0.5*b*(c_r - c_t); % [ft^2]
 
 % Determine number of of odd terms (N) for each percent error
 percent_error = [0.1; 0.01; 0.001];
@@ -43,11 +45,9 @@ for i = 1:length(percent_error)
     c_Di_error = 1;
 
     while c_L_error > percent_error(i)
-        N = N + 1;
-       
-        [e,c_L,c_Di] = PLLT(b, a0_t, a0_r, c_t, c_r, aero_t, aero_r, geo_t, geo_r, N);
+        [~,c_L,~] = PLLT(b, a0_t, a0_r, c_t, c_r, aero_t, aero_r, geo_t, geo_r, N);
         
-        c_L_error = abs((c_L_exact-c_L)/c_L_exact);
+        c_L_error = abs((c_L_exact(i)-c_L)/c_L_exact(i));
        
         if i == 1
             plot_c_L1(N) = c_L;
@@ -63,16 +63,15 @@ for i = 1:length(percent_error)
             plot_c_L3(N) = c_L;
             plot_NL3 = N;            
         end
+        N = N + 1;
     end
 
     N = 1;
 
     while c_Di_error > percent_error(i)
-        N = N + 1;
-       
-        [e,c_L,c_Di] = PLLT(b, a0_t, a0_r, c_t, c_r, aero_t, aero_r, geo_t, geo_r, N);
+        [~,~,c_Di] = PLLT(b, a0_t, a0_r, c_t, c_r, aero_t, aero_r, geo_t, geo_r, N);
         
-        c_Di_error = abs((c_Di_exact-c_Di)/c_Di_exact);
+        c_Di_error = abs((c_Di_exact(i)-c_Di)/c_Di_exact(i));
        
         if i == 1
             plot_c_Di1(N) = c_Di;
@@ -88,6 +87,7 @@ for i = 1:length(percent_error)
             plot_c_Di3(N) = c_Di;
             plot_ND3 = N;            
         end
+        N = N + 1;
     end
 end
 
@@ -99,6 +99,8 @@ Part3_Table1 = renamevars(Part3_Table1, "Var2", "NL");
 Part3_Table1 = renamevars(Part3_Table1, "Var3", "ND");
 Part3_Table1 = renamevars(Part3_Table1, "Var4", "C_L");
 Part3_Table1 = renamevars(Part3_Table1, "Var5", "C_Di");
+
+disp(Part3_Table1)
 
 %% Deliverable 2: (Plots --> c_L, c_Di vs N)
 figure() % c_L
@@ -131,16 +133,71 @@ ylabel("Coefficent of Induced Drag")
 title("Coefficent of Induced Drag Based on Odd Number of Terms for Different Error Values")
 legend(num2str(percent_error(3)),num2str(percent_error(2)),num2str(percent_error(1)), "Exact Value")
 
-% %% Deliverable 3: (Table 2)
-% V = 100; % [knots]
-% h = 10000; % [ft]
-% 
-% % Part3_Table2 = table(modes, Lift, D_i, plot_LD); 
-% % Part3_Table2 = renamevars(Part3_Table2, "Var1", "Relative Percent Error");
-% % Part3_Table2 = renamevars(Part3_Table2, "Var2", "N");
-% % Part3_Table2 = renamevars(Part3_Table2, "Var3", "C_L");
-% % Part3_Table2 = renamevars(Part3_Table2, "Var4", "C_Di");
-% 
-% %% Deliverable 4: (Plots --> C_D vs alpha)
-% 
-% %% Deliverable 5: (Plots --> L/D vs alpha
+%% Deliverable 3: (Table 2)
+V = 100 *1.68781; % [knots --> ft/s]
+h = 10000; % [ft]
+rho = 1.755*10^-3; % [slugs/ft] 
+c_L = plot_c_L3(end); % from previous part
+c_Di = plot_c_Di3(end);
+
+% Find cd using interpolation
+% clcd_2412 = readmatrix("2412clcd.mat");
+% clcd_0012 = readmatrix("0012clcd.mat");
+
+cd_0012 = 0.0066; % digitized value
+cd_2412 = 0.0075; % digitized value
+
+cd = (cd_0012+cd_2412)/2;
+
+L = 1/2*rho*V^2*S*c_L;
+D_i = 1/2*rho*V^2*S*c_Di;
+D = 1/2*rho*V^2*S*(cd+c_Di);
+
+LD_ratio = L./D;
+
+Part3_Table2 = table(percent_error(1), L, D_i, LD_ratio); 
+Part3_Table2 = renamevars(Part3_Table2, "Var1", "percent_error");
+
+disp(Part3_Table2)
+
+%% Deliverable 4: (Plots --> C_D vs alpha)
+alpha4 = linspace(-15,15,100);
+N = 200;
+for i = 1:length(alpha4)
+
+    [cl_r, aero_r4] = ThinAirfoil(c_r,m_r,p_r,Np,alpha4(i));
+    [cl_t, aero_t4] = ThinAirfoil(c_t,m_t,p_t,Np,alpha4(i));
+
+    % Account for geometric twist
+    geo_r_i = alpha4(i) + 1;
+    geo_t_i = alpha4(i) + 0;
+
+    [~,~,c_Di4(i)] = PLLT(b, a0_t, a0_r, c_t, c_r, aero_t4, aero_r4, geo_t_i, geo_r_i, N);
+end
+
+cd4 = cd*ones(size(alpha4));
+c_D_total = cd4 + c_Di4;
+
+figure()
+hold on;
+grid on;
+plot(alpha4,c_D_total)
+plot(alpha4, cd4)
+plot(alpha4, c_Di4)
+xlabel("Angle of Attack")
+ylabel("Coefficient of Total Drag")
+legend('Total Drag','Profile Drag','Induced Drag')
+title("Drag Breakdown Vs. Angle of Attack")
+
+%% Deliverable 5: (Plots --> L/D vs alpha)
+L5 = 1/2*rho*V^2*S*c_L.*ones(size(alpha4));
+D5 = 1/2*rho*V^2*S*(c_D_total);
+
+LD_ratio5 = L5./D5;
+
+figure()
+grid on;
+plot(alpha4, LD_ratio5)
+xlabel("Angle of Attack")
+ylabel("Lift Over Drag Ratio")
+title("LD Ratio Vs. Angle of Attack")
